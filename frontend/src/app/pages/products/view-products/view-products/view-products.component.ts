3e1396e6833788421c46fa13a3d01eff417a9d08
sm0k3rG <jhosepmarca.uta@gmail.com>
2024-09-09 18:56:44 -0300

    ver inventario v4

--- frontend/src/app/pages/products/view-products/view-products/view-products.component.ts
+++ frontend/src/app/pages/products/view-products/view-products/view-products.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import * as bootstrap from 'bootstrap';
-import { debounceTime } from 'rxjs/operators';
+import { Form, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Product } from '../../../../core/models/product';
 import { ProductService } from '../../../../core/services/product.service';
+import { PopoverModule } from '@coreui/angular';
+import { Popover } from 'bootstrap';
+
 
 
 
 @Component({
   selector: 'app-view-products',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, PopoverModule],
   templateUrl: './view-products.component.html',
   styleUrls: ['./view-products.component.css']
 })
@@ -19,8 +20,14 @@ import { ProductService } from '../../../../core/services/product.service';
 export class ViewProductsComponent implements OnInit {
   public products = new Array<Product>
 
-  constructor(private productService: ProductService) {
-    // Llamada al servicio en el constructor
+  forma!: FormGroup;
+
+  exampleStock: number = 0;
+
+  constructor(private productService: ProductService, private fb: FormBuilder) {
+    this.createForm();
+
+
     this.productService.getProducts().subscribe({
       next: (products: Product[]) => {
         this.products = products;
@@ -30,16 +37,65 @@ export class ViewProductsComponent implements OnInit {
       }
     });
   }
-  searchTerm: string = '';
+
+  createForm() {
+    this.forma = this.fb.group({
+
+      name: ['', [Validators.required]],
+      description: [''],
+      stock: ['', Validators.min(0)],
+      criticalStock: ['']
+
+    })
+  }
+  get notValidName(){
+
+    return this.forma.get('name')?.invalid && this.forma.get('name')?.touched;
+
+  }
+
+  preventNegative(event: KeyboardEvent) {
+    if (event.key === '-' || event.key === '+') {
+      event.preventDefault();
+    }
+  }
+
+  editProduct(product: Product) {
+    this.forma.patchValue({
+      name: product.name,
+      description: product.description,
+      stock: product.stock,
+      criticalStock: product.criticalStock
+    });
+  }
 
 
-  filteredTools = []
+  onSubmit() {
 
-  // get filteredItems() {
-  //   return this.products.filter(item =>
-  //     item.toLowerCase().includes(this.searchTerm.toLowerCase())
-  //   );
-  // }
+    if (this.forma.invalid){
+
+      return Object.values(this.forma.controls).forEach(control => {
+
+        control.markAllAsTouched();
+
+      });
+
+    }
+
+    return Object.values(this.forma.controls).forEach(control => {
+
+      control.reset();
+
+    });
+
+  }
+
+  searchTerm: string = ''
+    filteredList() {
+    return this.products.filter(products =>
+      products.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 
   deleteProduct(idProduct: number): void {
     this.products = this.products.filter(item => item.idProduct !== idProduct);
