647e29f72420f461d395c5322e1cf7eb8a0e4a9b
crissspi <cristinatiticortez@gmail.com>
2024-10-30 13:34:22 -0300

    feat: implementación cierre de sesión y arreglo de estilo login

--- frontend/src/app/pages/auth/login/login.component.ts
+++ frontend/src/app/pages/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { AuthService } from '../../../core/services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,7 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
   ngOnDestroy(): void {
   }
-  
+
   constructor(private authService: AuthService, private router: Router) {}
 
   public loginForm: FormGroup = new FormGroup({
@@ -25,14 +26,56 @@ export class LoginComponent implements OnInit, OnDestroy {
     password: new FormControl('', [Validators.required])
   });
 
+  get notValidUsername() {
+    return (
+      this.loginForm.get('username')?.invalid && this.loginForm.get('username')?.touched
+    );
+  }
+
+  get notValidPassword() {
+    return (
+      this.loginForm.get('password')?.invalid && this.loginForm.get('password')?.touched
+    );
+  }
+
+
   public login() {
+
+
+    if (this.loginForm.invalid) {
+      Object.values(this.loginForm.controls).forEach((control) => {
+        control.markAsTouched();
+      });
+      return;
+    }
+
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger me-2"
+      },
+      buttonsStyling: false
+    });
+
     this.authService.login(this.loginForm.value).subscribe({
       next: (result) => {
-        alert('Inicio de sesion correctamente');
+        swalWithBootstrapButtons.fire({
+          title: '¡Sesión iniciada!',
+          icon: 'success',
+          timer: 1500,
+          showConfirmButton: false,
+        });
         this.router.navigateByUrl('lendings/active');
       },
       error: (error) =>{
-        alert(error.message);
+        swalWithBootstrapButtons.fire({
+          title: 'Error',
+          text: 'El usuario o la contraseña son incorrectos',
+          icon: 'error',
+          timer: 1500,
+          showConfirmButton: false,
+        });
+        console.log(error.message);
       },
     });
   }
--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,7 +1,9 @@
+import { AuthService } from './../../../core/services/auth.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { AlertsComponent } from '../alerts/alerts.component';
 import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,4 +14,28 @@ import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class SidebarComponent {
   public show = true;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  public logout(): void {
+    const isLoggedOut = this.authService.logout();
+
+    if (isLoggedOut) {
+      Swal.fire({
+        title: '¡Sesión cerrada!',
+        icon: 'success',
+        timer: 1500,
+        showConfirmButton: false,
+      });
+      this.router.navigate(['auth/login']);
+    } else {
+      Swal.fire({
+        title: 'Error',
+        text: 'No se pudo cerrar la sesión',
+        icon: 'error',
+      });
+    }
+  }
+
+
 }
